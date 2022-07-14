a3d2d4cb8dc5395529cb2b7575de08cf0d60076f
Dan Wahlin <dwahlin@xmlforasp.net>
2022-07-14 16:39:32 -0700

    Add in adjustments for Fluid errors (need dev packages)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { FluidService } from './core/fluid.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
-  constructor() {}
+  
+  constructor(private fluidService: FluidService) {}
 
   ngOnInit(): void {}
 }
