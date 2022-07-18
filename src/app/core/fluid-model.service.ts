960eb0813d26eff3d44617b3a8bd077792b5ede9
Dan Wahlin <dwahlin@xmlforasp.net>
2022-07-18 12:43:54 -0700

    Cleanup

--- src/app/core/fluid-model.service.ts
+++ src/app/core/fluid-model.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { SharedMap } from 'fluid-framework';
-import { FluidContainerService } from './fluid-container.service';
-import { Item } from '../item';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { SignalManager } from '@fluid-experimental/data-objects';
+
+import { FluidContainerService } from './fluid-container.service';
+import { Item } from '../item';
 import { MouseTracker } from './mouse-tracker';
 import { FluidData } from '../shared/types';
 
@@ -17,8 +18,8 @@ export class FluidModelService {
     mouseTracker: MouseTracker | undefined;
     syncSharedMap: (() => void) | undefined;
     fluidData: FluidData | undefined;
-    private behaviorSubject$ = new BehaviorSubject({todoItems: [] as Item[], doneItems: [] as Item[]});
-    behaviorSubjectObservable$: Observable<{todoItems: Item[], doneItems: Item[]}>;
+    private behaviorSubject$ = new BehaviorSubject({ todoItems: [] as Item[], doneItems: [] as Item[] });
+    behaviorSubjectObservable$: Observable<{ todoItems: Item[], doneItems: Item[] }>;
 
     constructor(private fluidContainerService: FluidContainerService) {
         this.behaviorSubjectObservable$ = this.behaviorSubject$.asObservable();
@@ -49,17 +50,15 @@ export class FluidModelService {
     private syncTodosData() {
         // Only sync if the Fluid SharedMap object is defined.
         if (this.todosSharedMap) {
-          // TODO 4: Set the value of the localTimestamp state object that will appear in the UI.
-          this.syncSharedMap = () => { 
-            this.todoItems = this.todosSharedMap!.get('todoItems') ?? [];
-            this.doneItems = this.todosSharedMap!.get('doneItems') ?? [];
-            // Notify listeners that shared map data has changed.
-            this.behaviorSubject$.next({todoItems: this.todoItems, doneItems: this.doneItems});
-          };
-          this.syncSharedMap();
-    
-          // TODO 5: Register handlers.
-          this.todosSharedMap!.on('valueChanged', this.syncSharedMap);
+            this.syncSharedMap = () => {
+                this.todoItems = this.todosSharedMap!.get('todoItems') ?? [];
+                this.doneItems = this.todosSharedMap!.get('doneItems') ?? [];
+                // Notify listeners that shared map data has changed.
+                this.behaviorSubject$.next({ todoItems: this.todoItems, doneItems: this.doneItems });
+            };
+            this.syncSharedMap();
+
+            this.todosSharedMap!.on('valueChanged', this.syncSharedMap);
         }
-      }
+    }
 }
